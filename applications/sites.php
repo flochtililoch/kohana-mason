9a2ad401027dd62699eb7a9d46d2ccba07ab1fc6
florent <florent@f1d35d6a-7b8c-4c99-97d3-af093d0b4819>
2010-04-11 22:59:12 +0000

    moved applications into trunk
    git-svn-id: http://192.168.0.50:8000/svn/blah/trunk@305 f1d35d6a-7b8c-4c99-97d3-af093d0b4819

---
+++ applications/sites.php
@@ -0,0 +1,29 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	'brain' => array(
+		'appname'	=> 'brain',
+		'locale'	=> 'fr_FR',
+		'channel'	=> 1,
+		'env'		=> Kohana::DEVELOPMENT
+		),
+	'blah.fr' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'fr_FR',
+		'channel'	=> 1,
+		'env'		=> Kohana::DEVELOPMENT
+		),
+	'blah.es' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'es_ES',
+		'channel'	=> 2,
+		'env'		=> Kohana::DEVELOPMENT
+		),
+	'blah.co.uk' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'en_GB',
+		'channel'	=> 3,
+		'env'		=> Kohana::DEVELOPMENT
+		)
+);
