d46570c36f40e7e42cc3fc5bab05700d4714e529
florent <florent@f1d35d6a-7b8c-4c99-97d3-af093d0b4819>
2010-04-11 22:59:32 +0000

    moved public into trunk
    git-svn-id: http://192.168.0.50:8000/svn/blah/trunk@306 f1d35d6a-7b8c-4c99-97d3-af093d0b4819

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+// Define error reporting level
+error_reporting(E_ALL | E_STRICT);
+
+// Define the start time and memory usage of the application
+define('KOHANA_START_TIME', microtime(TRUE));
+define('KOHANA_START_MEMORY', memory_get_usage());
+
+// Define files extension
+define('EXT', '.php');
+
+// Define the absolute paths for configured directories
+define('DOCROOT', realpath(__DIR__.'/..').DIRECTORY_SEPARATOR);
+define('VENDORPATH', realpath(DOCROOT.'vendor').DIRECTORY_SEPARATOR);
+define('SYSPATH', realpath(VENDORPATH.'kohana').DIRECTORY_SEPARATOR);
+define('MODPATH', realpath(DOCROOT.'modules').DIRECTORY_SEPARATOR);
+define('VARPATH', realpath(DOCROOT.'var').DIRECTORY_SEPARATOR);
+define('APPSPATH', realpath(DOCROOT.'applications').DIRECTORY_SEPARATOR);
+
+// Load the base, core Kohana class and core extension
+require SYSPATH.'base'.EXT;
+require SYSPATH.'classes/kohana/core'.EXT;
+require MODPATH.'kohana'.EXT;
+
+// Init app
+Kohana::init();
