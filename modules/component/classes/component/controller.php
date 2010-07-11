674adb3235e5851c0fe14262cdf81336e024c5d8
florent <florent@f1d35d6a-7b8c-4c99-97d3-af093d0b4819>
2010-07-11 11:59:49 +0000

    cleaned / reorganised code execution
    git-svn-id: http://192.168.0.50:8000/svn/blah/trunk@387 f1d35d6a-7b8c-4c99-97d3-af093d0b4819

--- modules/component/classes/component/controller.php
+++ modules/component/classes/component/controller.php
@@ -35,52 +35,50 @@ class Component_Controller extends Kohana_Controller
     	    // Take the first component class name out of the components wrapping chain
     	    $comp = array_shift($base_comp::$_wrapping_chain);
 
-    		// If comp has attached views
-    		if(class_exists($comp))
-    		{
-				// Work with controller's entities only
-				$entities = Kohana::$tree['comps'][$comp::$_path][$comp::$_directory][$comp::$_name];
+			// Entities' path				
+			$path = $comp::$_path.'/'.$comp::$_directory.'_'.$comp::$_name;
+
+			// If the combination of assets for this type of execution has not been cached yet
+			if(! (Kohana::$caching === TRUE && $assets = Kohana::cache('assets_'.$path)) )
+			{
+				// Load assets for this specific component
+				$assets = Component::assets($comp);
+				
+				if(Kohana::$caching === TRUE)
+				{	
+					// Assets cache never expire
+					Kohana::cache('assets_'.$path, $assets, 0);
+				}
+			}
+
+			// Merge component's assets with main request current assets
+			if($comp::$_assets_pushed !== TRUE)
+			{
+				Request::$instance->assets = array_merge_recursive(Request::$instance->assets, $assets[$comp::$_assets_cache_key]);
+				
+				// Flag assets as pushed in the stack
+				$comp::$_assets_pushed = TRUE;
+			}
+
+			// If comp has attached views
+			if(array_key_exists('views', Kohana::$tree['comps'][$comp::$_path][$comp::$_directory][$comp::$_name]))
+			{
+				// Work with controller's views only
+				$views = Kohana::$tree['comps'][$comp::$_path][$comp::$_directory][$comp::$_name]['views'];
 				
 				// Find which class to use to load the view
 				$view_engine = $comp::$_view_engine ? $comp::$_view_engine : Kohana::config('view.engine');
 				
-				$path = $comp::$_path.'/'.$comp::$_directory.'_'.$comp::$_name;
-				
-				if(array_key_exists('views', Kohana::$tree['comps'][$comp::$_path][$comp::$_directory][$comp::$_name]))
-				{
-					// Build view object
-					$this->request->response = new $view_engine(
-						$path.'/views/'.$entities['views']['name'],
-						$comp,
-						$entities['views']['cache_id']
-						);
-				}
-
-				// If the combination of assets for this type of execution has not been cached yet
-				if(! (Kohana::$caching === TRUE && $assets = Kohana::cache('assets_'.$path)) )
-				{
-					// Load assets for this specific component
-					$assets = Component::assets($comp);
+				// Build view object
+				$this->request->response = new $view_engine(
+					$path.'/views/'.$views['name'],
+					$comp,
+					$views['cache_id']
+					);
 					
-					if(Kohana::$caching === TRUE)
-					{	
-						// Assets cache never expire
-						Kohana::cache('assets_'.$path, $assets, 0);
-					}
-				}
-
-				// Merge component's assets with main request current assets
-				if($comp::$_assets_pushed !== TRUE)
-				{
-					Request::$instance->assets = array_merge_recursive(Request::$instance->assets, $assets[$comp::$_assets_cache_key]);
-					
-					// Flag assets as pushed in the stack
-					$comp::$_assets_pushed = TRUE;
-				}
-
 				// Return view result
     			return $this->request;
-    		}
+			}
     		else
     		{
     			// Call next component view processing
