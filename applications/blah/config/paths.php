d00a136fb3c741c78ef0e432e9fb85f12039a37d
florent <florent@f1d35d6a-7b8c-4c99-97d3-af093d0b4819>
2010-08-22 12:37:14 +0000

    changed constant path name (vendor to lib)
    git-svn-id: http://192.168.0.50:8000/svn/blah/trunk@404 f1d35d6a-7b8c-4c99-97d3-af093d0b4819

--- applications/blah/config/paths.php
+++ applications/blah/config/paths.php
@@ -4,14 +4,14 @@ return array
 (
 	APPPATH,												// Application
 	CACHEPATH,												// Generated code
-	realpath(VENDORPATH.'symfony').DIRECTORY_SEPARATOR,		// Symfony components
+	realpath(LIBPATH.'symfony').DIRECTORY_SEPARATOR,		// Symfony components
 	realpath(MODPATH.'doctrine').DIRECTORY_SEPARATOR,		// Doctrine module
-	realpath(VENDORPATH.'doctrine').DIRECTORY_SEPARATOR,	// Doctrine ORM Library
+	realpath(LIBPATH.'doctrine').DIRECTORY_SEPARATOR,		// Doctrine ORM Library
 	realpath(MODPATH.'phptal').DIRECTORY_SEPARATOR,			// PHPTAL module
-	realpath(VENDORPATH.'phptal').DIRECTORY_SEPARATOR,		// PHPTAL Library
+	realpath(LIBPATH.'phptal').DIRECTORY_SEPARATOR,			// PHPTAL Library
 	realpath(MODPATH.'component').DIRECTORY_SEPARATOR,		// Component manager module
-	realpath(VENDORPATH.'cache').DIRECTORY_SEPARATOR,		// Cache module
+	realpath(LIBPATH.'cache').DIRECTORY_SEPARATOR,			// Cache module
 	realpath(MODPATH.'auth').DIRECTORY_SEPARATOR,			// Auth module extension
-	realpath(VENDORPATH.'auth').DIRECTORY_SEPARATOR,		// Auth module
+	realpath(LIBPATH.'auth').DIRECTORY_SEPARATOR,			// Auth module
 	SYSPATH													// Kohana core
 );
