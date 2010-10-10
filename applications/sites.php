87754f0f1b7a0d638342a62d92b0f562c9670120
florent <florent@f1d35d6a-7b8c-4c99-97d3-af093d0b4819>
2010-10-10 18:21:23 +0000

    added envs configurations
    git-svn-id: http://192.168.0.50:8000/svn/blah/trunk@433 f1d35d6a-7b8c-4c99-97d3-af093d0b4819

--- applications/sites.php
+++ applications/sites.php
@@ -2,7 +2,36 @@
 
 return array
 (
-	'blah.fr' => array(
+	// ****** DEVELOPMENT ******
+	'dev.blah.fr' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'fr_FR',
+		'channel'	=> 102,
+		'env'		=> Kohana::DEVELOPMENT,
+		'cdn'		=> array(
+			'dev.blah.fr/assets'
+			)	
+		),
+	'dev.blah.es' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'es_ES',
+		'channel'	=> 103,
+		'env'		=> Kohana::DEVELOPMENT,
+		'cdn'		=> array(
+			'dev.blah.es/assets'
+			)
+		),
+	'dev.blah.co.uk' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'en_GB',
+		'channel'	=> 4,
+		'env'		=> Kohana::DEVELOPMENT,
+		'cdn'		=> array(
+			'dev.blah.co.uk/assets'
+			)
+		),
+	// ****** TESTING ******
+	'test.blah.fr' => array(
 		'appname'	=> 'blah',
 		'locale'	=> 'fr_FR',
 		'channel'	=> 102,
@@ -13,11 +42,79 @@ return array
 			'blah.fr/a3'
 			)	
 		),
+	'test.blah.es' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'es_ES',
+		'channel'	=> 103,
+		'env'		=> Kohana::TESTING,
+		'cdn'		=> array(
+			'blah.es/assets',
+			'blah.es/assets',
+			'blah.es/assets'
+			)
+		),
+	'test.blah.co.uk' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'en_GB',
+		'channel'	=> 4,
+		'env'		=> Kohana::TESTING,
+		'cdn'		=> array(
+			'blah.co.uk/a1',
+			'blah.co.uk/a2',
+			'blah.co.uk/a3'
+			)
+		),
+	// ****** STAGING ******
+	'stag.blah.fr' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'fr_FR',
+		'channel'	=> 102,
+		'env'		=> Kohana::STAGING,
+		'cdn'		=> array(
+			'blah.fr/assets',
+			'blah.fr/assets',
+			'blah.fr/assets'
+			)	
+		),
+	'stag.blah.es' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'es_ES',
+		'channel'	=> 103,
+		'env'		=> Kohana::STAGING,
+		'cdn'		=> array(
+			'blah.es/assets',
+			'blah.es/assets',
+			'blah.es/assets'
+			)
+		),
+	'stag.blah.co.uk' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'en_GB',
+		'channel'	=> 4,
+		'env'		=> Kohana::STAGING,
+		'cdn'		=> array(
+			'blah.co.uk/assets',
+			'blah.co.uk/assets',
+			'blah.co.uk/assets'
+			)
+		),
+	// ****** PRODUCTION ******
+	'blah.fr' => array(
+		'appname'	=> 'blah',
+		'locale'	=> 'fr_FR',
+		'channel'	=> 102,
+		'env'		=> Kohana::PRODUCTION,
+		'cdn'		=> array(
+			'blah.fr/a1',
+			'blah.fr/a2',
+			'blah.fr/a3'
+			)	
+		),
 	'blah.es' => array(
 		'appname'	=> 'blah',
 		'locale'	=> 'es_ES',
 		'channel'	=> 103,
-		'env'		=> Kohana::DEVELOPMENT,
+		'env'		=> Kohana::PRODUCTION,
 		'cdn'		=> array(
 			'blah.es/assets',
 			'blah.es/assets',
@@ -28,7 +125,7 @@ return array
 		'appname'	=> 'blah',
 		'locale'	=> 'en_GB',
 		'channel'	=> 4,
-		'env'		=> Kohana::TESTING,
+		'env'		=> Kohana::PRODUCTION,
 		'cdn'		=> array(
 			'blah.co.uk/a1',
 			'blah.co.uk/a2',
