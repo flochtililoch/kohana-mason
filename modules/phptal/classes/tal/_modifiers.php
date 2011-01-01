c7bce1567ce8861ae9fa7f7a2a2c371e431a8ac9
florent <florent@f1d35d6a-7b8c-4c99-97d3-af093d0b4819>
2011-01-01 23:50:48 +0000

    fixes tales in comp & static modifiers
    git-svn-id: http://192.168.0.50:8000/svn/blah/trunk@478 f1d35d6a-7b8c-4c99-97d3-af093d0b4819

--- modules/phptal/classes/tal/_modifiers.php
+++ modules/phptal/classes/tal/_modifiers.php
@@ -13,7 +13,7 @@
 function phptal_tales_comp($paths, $nothrow)
 {
 	// Convert paths to array
-	$paths = explode(' ', trim($paths));
+	$paths = explode(';', trim($paths));
 	
 	// First string in paths is a comp
 	$comp = array_shift($paths);
@@ -23,17 +23,7 @@ function phptal_tales_comp($paths, $nothrow)
 		$params = array();
 		foreach($paths as $path)
 		{
-			$segments = explode('/', $path);
-			$obj = '$ctx->'.array_shift($segments);
-			if(count($segments))
-			{
-				$params[] = '\''.$segments[count($segments)-1].'\' => $ctx->path('.$obj.', \''.implode('/', $segments).'\')';
-			}
-			// If there's just one object path, no needs to run path() on it
-			else
-			{
-				$params[] ='\''.$path.'\' => '.$obj;
-			}
+			$params[] = phptal_tale($path, $nothrow);
 		}
 		$params = ', array('.implode(', ', $params).')';	
 	}
@@ -65,7 +55,7 @@ function phptal_tales_orm($path, $nothrow)
 function phptal_tales_static($paths, $nothrow)
 {
 	// Convert path to array
-	$paths = explode(' ', trim($paths));
+	$paths = explode(';', trim($paths));
 	
 	// First string in path is class name/method_name
 	$static = explode('/', array_shift($paths));
@@ -77,17 +67,7 @@ function phptal_tales_static($paths, $nothrow)
 		$params = array();
 		foreach($paths as $path)
 		{
-			$segments = explode('/', $path);
-			$obj = '$ctx->'.array_shift($segments);
-			if(count($segments))
-			{
-				$params[] = '$ctx->path('.$obj.', \''.implode('/', $segments).'\')';
-			}
-			// If there's just one object path, no needs to run path() on it
-			else
-			{
-				$params[] ='\''.$path.'\'';
-			}
+			$params[] = phptal_tale($path, $nothrow);
 		}
 		$params = implode(', ', $params);
 	}
