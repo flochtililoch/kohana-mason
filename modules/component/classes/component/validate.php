2a6c65f0eb4e01e2b789b17354fd3d4ce8fa8202
florent <florent@f1d35d6a-7b8c-4c99-97d3-af093d0b4819>
2011-02-01 23:21:40 +0000

    Factorized validation check. Can be used with doctrine entities. Should be tested with other data gathering forms
    git-svn-id: http://192.168.0.50:8000/svn/blah/trunk@496 f1d35d6a-7b8c-4c99-97d3-af093d0b4819

---
+++ modules/component/classes/component/validate.php
@@ -0,0 +1,68 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Kohana's Validate Class Extension
+ *
+ * @package    Component
+ * @author     Florent Bonomo
+ */
+class Component_Validate extends Kohana_Validate
+{
+	public function process($rules = array(), $filters = array(), $callback_field_valid = NULL)
+	{
+		foreach($this as $field => $value)
+		{
+			if(isset($filters) && array_key_exists($field, $filters))
+			{
+				if(is_array($filters[$field]))
+				{
+					$this->filters($field, $filters[$field]);
+				}
+				else
+				{
+					$this->filter($field, $filters[$field]);
+				}
+			}
+			if(isset($rules) && array_key_exists($field, $rules))
+			{
+				if(is_array($rules[$field]))
+				{
+					$this->rules($field, $rules[$field]);
+				}
+				else
+				{
+					$this->rule($field, $rules[$field]);
+				}
+			}
+		}
+
+		if($this->check())
+		{
+			foreach($this->as_array() as $p => $v)
+			{
+				if(array_key_exists($p, $rules))
+				{
+					if(is_array($rules[$p]) && in_array('date', $rules[$p]) || $rules[$p] === 'date')
+					{
+						$date_format = array_key_exists($p.'_datetype', $this) ? $this[$p.'_datetype'] : I18n::DATE_TYPE;
+						$time_format = array_key_exists($p.'_timetype', $this) ? $this[$p.'_timetype'] : I18n::TIME_TYPE;
+						unset($this[$p.'_datetype'], $this[$p.'_timetype']);
+						$v = I18n::datetime($v, $date_format, $time_format)->datetime;
+					}
+				}
+				if(is_callable($callback_field_valid))
+				{
+					$callback_field_valid($p, $v);
+				}
+			}
+			return TRUE;
+		}
+		else
+		{
+			return array(
+				'errors' => $this->errors('validate'),
+				'data' => $this->as_array()
+				);
+		}
+	}
+}
